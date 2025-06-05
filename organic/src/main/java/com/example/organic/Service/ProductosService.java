4cfc0ce2d3bcaf4ed731cec4abc8ee36266c66da
Ghost2647 <134562819+DogseGhost2647@users.noreply.github.com>
2025-06-05 13:14:10 -0500

    Repositorio, idao y ProductosService añadidos

--- organic/src/main/java/com/example/organic/Service/ProductosService.java
+++ organic/src/main/java/com/example/organic/Service/ProductosService.java
@@ -1,5 +1,49 @@
 package com.example.organic.Service;
 
-public class ProductosService {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.organic.Entity.ProductosEntity;
+import com.example.organic.Repository.ProductosRepository;
+import com.example.organic.Service.DAO.IDAO;
+
+public class ProductosService implements IDAO<ProductosEntity, Long> {
+
+    @Autowired
+    private IDAO idao;
+
+    @Autowired
+    private ProductosRepository productosRepository;
+
+    @Override
+    public List<ProductosEntity> getAll() {
+        return productosRepository.findAll();
+    }
+
+    @Override
+    public ProductosEntity getById(Long id) {
+        return productosRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public ProductosEntity create(ProductosEntity entity){
+        return productosRepository.save(entity);
+    }
+
+    @Override
+    public ProductosEntity update(ProductosEntity entity) {
+        if (productosRepository.existsById(entity.getId())) {
+            return productosRepository.save(entity);
+        }
+        return null; // or throw an exception
+    }
+    
+    @Override
+    public void delete(Long id) {
+        if (productosRepository.existsById(id)) {
+            productosRepository.deleteById(id);
+        }
+    }
 
 }
