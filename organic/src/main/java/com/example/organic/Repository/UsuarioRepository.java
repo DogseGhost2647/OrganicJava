f922a9667abac83342f3743091b26f386c79b54b
jose <jodadima09@gmail.com>
2025-09-28 15:22:22 -0500

    Borrar carpeta extra

--- organic/src/main/java/com/example/organic/Repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.organic.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.organic.Entity.UsuarioEntity;
-
-public interface UsuarioRepository extends JpaRepository <UsuarioEntity, Long> {
-    UsuarioEntity findByCorreo(String correo);
-}
